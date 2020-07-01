00bee35430daaac5fa6e74f17e72db0131504803
johnsonbrentley <johnsonbrentley8@gmail.com>
2020-06-30 20:04:45 -0400

    Fixing directories

--- Projects/Project2/blinker.sh
+++ Projects/Project2/blinker.sh
@@ -2,10 +2,10 @@
 #ACT LED
 cd /sys/class/leds/led0
 sudo sh -c "echo timer > trigger"
-sudo sh -c "echo 500 > delay_on"
-sudo sh -c "echo 500 > delay_off"
+sudo sh -c "echo 50 > delay_on"
+sudo sh -c "echo 50 > delay_off"
 #PWR LED
 cd /sys/class/leds/led1
 sudo sh -c "echo timer > trigger"
-sudo sh -c "echo 500 > delay_on"
-sudo sh -c "echo 500 > delay_off"
+sudo sh -c "echo 50 > delay_on"
+sudo sh -c "echo 50 > delay_off"
